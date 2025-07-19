3cd9d1457b9a1a1a60246476efa515caed9aff18
Pedro Ezequiel <pedro_alonso_ezequiel@hotmail.com>
2025-07-19 09:28:01 -0300

    render test

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const app = express();
+
+let notes = [
+    {
+        id: "1",
+        content: "HTML is easy",
+        important: true
+    },
+    {
+        id: "2",
+        content: "Browser can execute only JavaScript",
+        important: false
+    },
+    {
+        id: "3",
+        content: "GET and POST are the most important methods of HTTP protocol",
+        important: true
+    }
+];
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method);
+    console.log('Path:', request.path);
+    console.log('Body:', request.body);
+    console.log('---');
+    next();
+}
+
+app.use(express.json());
+
+app.use(cors({ origin: 'http://localhost:5173' }));
+//app.use(requestLogger);
+
+morgan.token("body", (req) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hellowwworld...</h1>')
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const note = notes.find(n => n.id === id);
+
+    if (note) {
+        res.json(note);
+    } else {
+        res.status(404).end();
+    }
+});
+
+app.post('/api/notes', (req, res) => {
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => Number(n.id)))
+        : 0;
+
+    const note = req.body;
+    note.id = String(maxId + 1);
+
+    notes = notes.concat(note);
+
+    res.json(note);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    const id = req.params.id;
+    notes = notes.filter(n => n.id !== id);
+
+    res.status(204).end();
+});
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' });
+}
+
+app.use(unknownEndpoint);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
