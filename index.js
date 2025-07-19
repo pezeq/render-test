78564e54f123311a3ada33c1337d7607ca51f5d8
Pedro Ezequiel <pedro_alonso_ezequiel@hotmail.com>
2025-07-19 10:10:57 -0300

    render-test / added dist folder

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors');
 const app = express();
 
 let notes = [
@@ -21,6 +20,8 @@ let notes = [
     }
 ];
 
+app.use(express.json());
+
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method);
     console.log('Path:', request.path);
@@ -28,9 +29,12 @@ const requestLogger = (request, response, next) => {
     console.log('---');
     next();
 }
-
-app.use(express.json());
 app.use(requestLogger);
+
+morgan.token("body", (req) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
+app.use(express.static('dist'));
 
 app.get('/', (req, res) => {
     res.send('<h1>Hellowwworld...</h1>')
@@ -74,7 +78,6 @@ app.delete('/api/notes/:id', (req, res) => {
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' });
 }
-
 app.use(unknownEndpoint);
 
 const PORT = process.env.PORT || 3001;
